4619296f11742392e937e2c5ff84d92ac6e561e6
munafBCloud <munaf8289@gmail.com>
2026-06-29 17:34:32 +0000

    Add API Gateway

---
+++ api_gateway.tf
@@ -0,0 +1,10 @@
+resource "aws_apigatewayv2_api" "ynj_api" {
+  name          = "${var.project_name}-${var.environment}-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers = ["*"]
+  }
+}
